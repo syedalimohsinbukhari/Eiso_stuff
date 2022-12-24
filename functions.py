06a2c912c09d3c14929040c404285ea4d9bb0f06
Astrophysics And Python <astrophysicsandpython@gmail.com>
2022-12-24 15:28:29 +0500

    [MINOR-1]
    * did some minor correction to functions, spectral_models.py and .gitignore

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
 """
 Created on Tue Feb 22 09:52:10 2022
 """
+
 import copy
 from multiprocessing import Pool
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import scipy.integrate as si
 from astropy.cosmology import FlatLambdaCDM
 
@@ -13,77 +15,137 @@ from spectral_models import SpectralModels
 
 
 def get_parameter_details(model_name):
-    if model_name.lower() == 'pl':
+    model_name = model_name.lower()
+
+    if model_name == 'pl':
         n_par, par_n = 3, ['a_pl', 'i1_pl', 'epiv_pl']
-    elif model_name.lower() == 'pl_bb':
+    elif model_name == 'pl_bb':
         n_par, par_n = 5, ['a_pl', 'i1_pl', 'epiv_pl', 'a_bb', 'kt_bb']
-    elif model_name.lower() == 'sbpl':
+    elif model_name == 'sbpl':
         n_par, par_n = 6, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl']
-    elif model_name.lower() == 'sbpl_pl':
-        n_par, par_n = 9, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl', 'a_pl', 'i1_pl', 'epiv_pl']
-    elif model_name.lower() == 'sbpl_bb':
-        n_par, par_n = 8, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl', 'a_bb', 'kt_bb']
-    elif model_name.lower() == 'sbpl_pl_bb':
-        n_par, par_n = 11, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl', 'amp_pl', 'i1_pl', 'epiv_pl', 'a_bb', 'kt_bb']
-    elif model_name.lower() == 'band':
+    elif model_name == 'sbpl_pl':
+        n_par, par_n = 9, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl',
+                           'a_pl', 'i1_pl', 'epiv_pl']
+    elif model_name == 'sbpl_bb':
+        n_par, par_n = 8, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl',
+                           'a_bb', 'kt_bb']
+    elif model_name == 'sbpl_pl_bb':
+        n_par, par_n = 11, ['a_sbpl', 'epiv_sbpl', 'i1_sbpl', 'bE', 'bS', 'i2_sbpl',
+                            'amp_pl', 'i1_pl', 'epiv_pl', 'a_bb', 'kt_bb']
+    elif model_name == 'band':
         n_par, par_n = 4, ['a_band', 'ep_band', 'i1_band', 'i2_band']
-    elif model_name.lower() == 'band_pl':
-        n_par, par_n = 7, ['a_band', 'ep_band', 'i1_band', 'i2_band', 'a_pl', 'i1_pl', 'epiv_pl']
-    elif model_name.lower() == 'band_bb':
+    elif model_name == 'band_pl':
+        n_par, par_n = 7, ['a_band', 'ep_band', 'i1_band', 'i2_band', 'a_pl', 'i1_pl',
+                           'epiv_pl']
+    elif model_name == 'band_bb':
         n_par, par_n = 6, ['a_band', 'ep_band', 'i1_band', 'i2_band', 'a_bb', 'kt_bb']
-    elif model_name.lower() == 'band_pl_bb':
-        n_par, par_n = 9, ['a_band', 'ep_band', 'i1_band', 'i2_band', 'a_pl', 'i1_pl', 'epiv_pl', 'amp_bb', 'kt_bb']
-    elif model_name.lower() == 'cpl':
+    elif model_name == 'band_pl_bb':
+        n_par, par_n = 9, ['a_band', 'ep_band', 'i1_band', 'i2_band', 'a_pl', 'i1_pl',
+                           'epiv_pl', 'amp_bb', 'kt_bb']
+    elif model_name == 'cpl':
         n_par, par_n = 4, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl']
-    elif model_name.lower() == 'cpl_pl':
-        n_par, par_n = 7, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl', 'a_pl', 'i1_pl', 'epiv_pl']
-    elif model_name.lower() == 'cpl_bb':
+    elif model_name == 'cpl_pl':
+        n_par, par_n = 7, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl', 'a_pl', 'i1_pl',
+                           'epiv_pl']
+    elif model_name == 'cpl_bb':
         n_par, par_n = 6, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl', 'a_bb', 'kt_bb']
-    elif model_name.lower() == 'cpl_pl_bb':
-        n_par, par_n = 9, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl', 'a_pl', 'i1_pl', 'epiv_pl', 'amp_bb', 'kt_bb']
+    elif model_name == 'cpl_pl_bb':
+        n_par, par_n = 9, ['a_cpl', 'ep_cpl', 'i1_cpl', 'epiv_cpl', 'a_pl', 'i1_pl',
+                           'epiv_pl', 'amp_bb', 'kt_bb']
     else:
         n_par, par_n = 0, []
 
     return n_par, par_n
 
 
-def get_parameter_values(fit_extension, number_of_parameters):
-    pars_ = [fit_extension.data[f'PARAM{i}'][0][0] for i in range(number_of_parameters)]
-    errs_ = [fit_extension.data[f'PARAM{i}'][0][1] for i in range(number_of_parameters)]
+def get_parameter_values(fit_extension, n_parameters):
+    pars_ = [fit_extension.data[f'PARAM{i}'][0][0] for i in range(n_parameters)]
+    errs_ = [fit_extension.data[f'PARAM{i}'][0][1] for i in range(n_parameters)]
 
-    covariance_matrix = fit_extension.data['COVARMAT'][0][0:number_of_parameters, 0:number_of_parameters]
+    covariance_matrix = fit_extension.data['COVARMAT'][0][0:n_parameters, 0:n_parameters]
 
     return pars_, errs_, covariance_matrix
 
 
 def get_median_values(mvd_array, sigma):
     _median = np.median(mvd_array)
-    hi_mask, lo_mask = mvd_array >= _median, mvd_array < _median
-    hi, lo = np.sort(mvd_array[hi_mask]), np.sort(mvd_array[lo_mask])
-    _norm_hi, _norm_lo = hi[np.int64(sigma * np.size(hi))], lo[np.int64((1 - sigma) * np.size(lo))]
+
+    hi_mask = mvd_array >= _median
+    lo_mask = mvd_array < _median
+
+    hi = np.sort(mvd_array[hi_mask])
+    lo = np.sort(mvd_array[lo_mask])
+
+    _norm_hi = hi[np.int64(sigma * np.size(hi))]
+    _norm_lo = lo[np.int64((1 - sigma) * np.size(lo))]
+
     return _median, _norm_hi, _norm_lo
 
 
-def parameter_plotting(mvd, number_of_parameters, parameter_names, n_bins, sigma):
-    _f = (10, 5) if 'e_isotropic' in parameter_names else (20, 5)
+def x_labels(axis_thing, parameter_name):
+    if parameter_name in ['a_sbpl', 'a_band', 'a_pl', 'a_cpl']:
+        out = 'Amplitude\n[ph/cm' + r'$^2$' + '/s]'
+    if parameter_name in ['i1_sbpl', 'i1_band', 'i1_cpl']:
+        out = r'$\alpha$'
+    elif parameter_name in ['i2_sbpl', 'i2_band']:
+        out = r'$\beta$'
+    elif parameter_name == 'bE':
+        out = 'Break energy\n[keV]'
+    elif parameter_name == 'e_i_peak':
+        out = r'E$_\mathrm{i,\ peak}$' + '\n[keV]'
+    elif parameter_name == 'e_flux':
+        out = 'Flux\n[erg/cm' + r'$^2$' + '/s]'
+    elif parameter_name == 'e_flnc':
+        out = 'Fluence\n[erg/cm' + r'$^2$]'
+    elif parameter_name == 'e_isotropic':
+        out = r'E$_\mathrm{iso}$' + '\n[erg]'
+    elif parameter_name in ['ep_band', 'ep_cpl']:
+        out = r'E$_\mathrm{peak}$ [keV]'
+    else:
+        out = ''
 
-    f, ax = plt.subplots(1, number_of_parameters, figsize=_f, sharey='row')
+    axis_thing.set_xlabel(out)
+
+
+def parameter_plotting(mvd, number_of_parameters, parameter_names, n_bins, sigma):
+    f, ax = plt.subplots(1, number_of_parameters, figsize=(20, 5), sharey='row')
+
+    if 'e_isotropic' not in parameter_names:
+        color = ['tab:blue'] * len(parameter_names)
+    else:
+        color = ['lime', 'lightcoral', 'gold', 'gold']
+
+    p, m, nl, nh = [], [], [], []
+
     for number, parameter in zip(range(number_of_parameters), parameter_names):
-        ax[number].hist(mvd[parameter], bins=n_bins, color='g')
+        ax[number].hist(mvd[parameter], bins=n_bins, color=color[number])  # , alpha=0.5)
         _median, _norm_hi, _norm_lo = get_median_values(mvd[parameter], sigma)
-        [ax[number].axvline(value, color='k', ls=line_style) for value, line_style in zip([_median, _norm_lo, _norm_hi], ['-', '-.', ':'])]
-        if not parameter == 'e_isotropic':
-            ax[number].plot([], [], label=r'%.4f$_{-%.3E}^{+%.3E}$' % (_median, _median - _norm_lo, _norm_hi - _median), color='w')
+        [ax[number].axvline(value, color='k', ls=line_style) for value, line_style in
+         zip([_median, _norm_lo, _norm_hi], ['-', '-.', ':'])]
+        if parameter not in ['e_isotropic', 'e_flux', 'e_flnc']:
+            ax[number].plot([], [], label=r'%.4f$_{-%.3E}^{+%.3E}$' % (
+                _median, _median - _norm_lo, _norm_hi - _median), color='w')
         else:
-            ax[number].plot([], [], label=r'%.4E$_{-%.3E}^{+%.3E}$' % (_median, _median - _norm_lo, _norm_hi - _median), color='w')
+            ax[number].plot([], [], label=r'%.4E$_{-%.3E}^{+%.3E}$' % (
+                _median, _median - _norm_lo, _norm_hi - _median), color='w')
         if number == 0:
             ax[number].set_ylabel('Counts')
-        ax[number].set_xlabel(f'{parameter}')
+
+        x_labels(ax[number], parameter_name=parameter)
+
         ax[number].legend(loc='best', frameon=False)
         if number > 0:
-            # taken from https://www.geeksforgeeks.org/how-to-remove-ticks-from-matplotlib-plots/
+            # taken from https://www.geeksforgeeks.org/how-to-remove-ticks-from
+            # -matplotlib-plots/
             ax[number].tick_params(left=False)
+        p.append(parameter)
+        m.append(_median)
+        nl.append(_median - _norm_lo)
+        nh.append(_norm_hi - _median)
+
     plt.tight_layout()
+
+    return p, m, nl, nh
 
 
 def break_energy_to_peak_energy__sbpl(i1_sbpl, break_energy, break_scale, i2_sbpl):
@@ -92,7 +154,8 @@ def break_energy_to_peak_energy__sbpl(i1_sbpl, break_energy, break_scale, i2_sbp
 
 
 class IsotropicEnergy:
-    def __init__(self, model_name, multivariate_dictionary, sigma, n_iterations, t_start, t_stop, redshift=0, e_low=8, e_high=int(1e7), h0=67.4, omega_m=0.315):
+    def __init__(self, model_name, multivariate_dictionary, sigma, n_iterations, t_start,
+                 t_stop, redshift=0, e_low=8, e_high=int(1e7), h0=67.4, omega_m=0.315):
         self.model_name = model_name.lower()
         self.multivariate_dictionary = copy.deepcopy(multivariate_dictionary)
         self.sigma = sigma
@@ -115,42 +178,138 @@ class IsotropicEnergy:
     def __duration(self):
         return self.t_stop - self.t_start
 
-    def __luminosity_distance(self, int_z=0):
-        return FlatLambdaCDM(H0=self.h0, Om0=self.omegaM).luminosity_distance(int_z).cgs.value
+    def __luminosity_distance(self, int_z):
+        return FlatLambdaCDM(H0=self.h0, Om0=self.omegaM).luminosity_distance(
+            int_z).cgs.value
 
     def __luminosity_integral(self):
         return si.quad(self.__luminosity_distance, 0, self.redshift)[0]
 
-    def isotropic_energy(self, mvd_with_dl):
-        e_range = np.logspace(np.log10(self.e_low), np.log10(self.e_high), int(self.n_iterations))
+    def __energy_range(self):
+        return np.logspace(np.log10(self.e_low), np.log10(self.e_high),
+                           int(self.n_iterations))
 
-        _fluence = SpectralModels(e_range, mvd_with_dl[:-1], self.t_start, self.t_stop, self.model_name, 'bolometric').get_values()
+    def __generic(self, pars, _type='bolometric'):
+        e_range = self.__energy_range()
+        return SpectralModels(e_range, pars[:-1], self.t_start, self.t_stop,
+                              self.model_name, _type).get_values()
 
-        _constant = (4 * np.pi * mvd_with_dl[-1]**2) * (1 + self.redshift)**-1
+    @staticmethod
+    def __generic_mp(func, n_proc, chunks):
+        return np.array([Pool(n_proc).map(func, chunk) for chunk in chunks]).flatten()
+
+    def get_flux(self, pars):
+        return self.__generic(pars, 'integrate')
+
+    def get_fluence(self, pars):
+        return self.__generic(pars, 'integrate') * self.__duration()
+
+    def isotropic_energy(self, pars):
+        _fluence = self.__generic(pars, _type='bolometric')
+
+        _constant = (4 * np.pi * pars[-1]**2) * (1 + self.redshift)**-1
+
         return _fluence * self.__duration() * _constant
 
-    def isotropic_energy__mp(self, luminosity_distance, n_proc):
+    def get_value_error_pairs(self, output_type='isotropic', use_multiprocessing=False,
+                              n_proc=2):
         mvd = self.multivariate_dictionary
+        n_proc = 1 if not use_multiprocessing else n_proc
 
-        mvd['_dl'] = np.repeat(luminosity_distance, self.n_iterations)
+        if output_type == 'isotropic':
+            mvd['_dl'] = np.repeat(self.__luminosity_integral(), self.n_iterations)
 
         mvd = np.array(list(mvd.values())).T
 
         _chunks = tuple(self.__chunks(mvd, self.n_iterations // n_proc))
 
-        return [Pool(n_proc).map(self.isotropic_energy, chunk) for chunk in _chunks]
+        if output_type == 'isotropic':
+            out = self.__generic_mp(func=self.isotropic_energy, n_proc=n_proc,
+                                    chunks=_chunks)
+        elif output_type == 'fluence':
+            out = self.__generic_mp(func=self.get_fluence, n_proc=n_proc, chunks=_chunks)
+        else:
+            out = self.__generic_mp(func=self.get_flux, n_proc=n_proc, chunks=_chunks)
 
-    def get_value_error_pairs(self, use_multiprocessing=False, n_proc=2):
+        return out
 
-        luminosity_distance = self.__luminosity_integral()
 
-        n_proc = 1 if not use_multiprocessing else n_proc
+def save_val_err(arr1, arr2, path, model_name, suffix):
+    t1 = pd.DataFrame(arr1[1:])
+    t2 = pd.DataFrame(arr2[1:])
+    t1.columns = arr1[0]
+    t2.columns = arr2[0]
 
-        e_isotropic = self.isotropic_energy__mp(luminosity_distance, n_proc=n_proc)
+    t3 = pd.concat([t1, t2], axis=1)
 
-        _values = np.array(e_isotropic)
+    t3.to_csv(f'{path}/{model_name.lower()}__parameter_value_err{suffix}.csv',
+              index=False)
 
-        values = np.array([value[:, 0] for value in _values]).flatten()
-        errors = np.array([error[:, 1] for error in _values]).flatten()
 
-        return values, errors
+def make_dataframe(data_frame, m_name):
+    wo_peak = ['e_flux', 'e_flnc', 'e_isotropic']
+    w_peak = ['e_flux', 'e_flnc', 'e_i_peak', 'e_isotropic']
+
+    pl = ['a_pl', 'i1_pl']
+    bb = ['a_bb', 'kt_bb']
+    sbpl = ['a_sbpl', 'i1_sbpl', 'bE', 'i2_sbpl']
+    band = ['a_band', 'ep_band', 'i1_band', 'i2_band']
+    cpl = ['a_cpl', 'i1_cpl', 'ep_cpl']
+
+    if m_name == 'pl':
+        pl += wo_peak
+        df_ = data_frame[pl]
+    elif m_name == 'pl_bb':
+        pl += bb
+        pl += wo_peak
+        df_ = data_frame[pl]
+    elif m_name == 'sbpl':
+        sbpl += w_peak
+        df_ = data_frame[sbpl]
+    elif m_name == 'sbpl_pl':
+        pl += sbpl
+        pl += w_peak
+        df_ = data_frame[pl]
+    elif m_name == 'sbpl_bb':
+        sbpl += bb
+        sbpl += w_peak
+        df_ = data_frame[sbpl]
+    elif m_name == 'sbpl_pl_bb':
+        pl += sbpl
+        pl += bb
+        pl += w_peak
+        df_ = data_frame[pl]
+    elif m_name == 'cpl':
+        cpl += w_peak
+        df_ = data_frame[cpl]
+    elif m_name == 'cpl_pl':
+        pl += cpl
+        pl += w_peak
+        df_ = data_frame[pl]
+    elif m_name == 'cpl_bb':
+        cpl += bb
+        cpl += w_peak
+        df_ = data_frame[cpl]
+    elif m_name == 'cpl_pl_bb':
+        pl += cpl
+        pl += bb
+        pl += w_peak
+        df_ = data_frame[pl]
+    elif m_name == 'band':
+        band += w_peak
+        df_ = data_frame[band]
+    elif m_name == 'band_pl':
+        pl += band
+        pl += w_peak
+        df_ = data_frame[pl]
+    elif m_name == 'band_bb':
+        band += bb
+        band += w_peak
+        df_ = data_frame[band]
+    else:
+        pl += band
+        pl += bb
+        pl += w_peak
+        df_ = data_frame[pl]
+
+    return df_
--- spectral_models.py
+++ spectral_models.py
@@ -29,8 +29,7 @@ BAND_PL_BB: PL + BAND + BB
 
 class SpectralModels:
 
-    def __init__(self, energy, pars, t_start, t_stop, model: str = 'pl',
-                 model_type: str = 'counts', flux_energy=None,
+    def __init__(self, energy, pars, t_start, t_stop, model: str = 'pl', model_type: str = 'counts', flux_energy=None,
                  isotropic_energy=None, redshift=None, h0=67.4, omega_m=0.315):
 
         self.energy = energy
@@ -54,8 +53,7 @@ class SpectralModels:
         self.__raise_error()
 
     def __luminosity_distance(self, z_int):
-        return FlatLambdaCDM(H0=self.h0,
-                             Om0=self.omega_m).luminosity_distance(z_int).cgs.value
+        return FlatLambdaCDM(H0=self.h0, Om0=self.omega_m).luminosity_distance(z=z_int).cgs.value
 
     def __luminosity_integral(self):
         return si.quad(self.__luminosity_distance, 0, self.z)[0]
@@ -63,80 +61,94 @@ class SpectralModels:
     def __raise_error(self):
         types_ = ['counts', 'energy', 'integrate', 'bolometric']
         if self.model_type not in types_:
-            raise TypeError('Model type must be in ' + ', '.join(types_[:-1]) +
-                            f' or {types_[-1]}.')
+            raise TypeError(f'Model type must be in {", ".join(types_[:-1])} or {types_[-1]}.')
 
-    def __time_difference(self):
+    def __get_time_difference(self):
         return self.t_stop - self.t_start
 
-    def __vals(self):
-        return self.energy, self.pars, self.model_type, self.f_ene, self.f_iso, self.z, \
-               self.keVtoErg
+    def __values(self):
+        return self.energy, self.pars, self.model_type, self.f_ene, self.f_iso, self.z, self.keVtoErg
 
     def powerlaw(self, joint_pars=None):
-        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__vals()
+        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__values()
 
         pars = pars if joint_pars is None else joint_pars
 
         def f_pl(_e=energy, out_ene=True):
             [amp, e_piv, ind] = pars
-            out = amp * (_e * e_piv**-1)**ind
+            res = amp * (_e * e_piv**-1)**ind
 
-            return _e * out if out_ene else out
+            return _e * res if out_ene else res
 
         def int_pl(_range=f_ene):
             return si.quad(f_pl, _range[0], _range[1])[0] * kev_
 
         def bol_pl():
             _bol_range = [i / (1 + z) for i in f_iso]
-            return int_pl(_range=_bol_range) * self.__time_difference()
+            return int_pl(_range=_bol_range) * self.__get_time_difference()
 
-        return f_pl(out_ene=False) if mt == 'counts' else f_pl() if \
-            mt == 'energy' else int_pl() if mt == 'integrate' else bol_pl()
+        func_map = {'counts': f_pl(out_ene=False),
+                    'energy': f_pl(),
+                    'integrate': int_pl(),
+                    'bolometric': bol_pl()}
+
+        return func_map[mt]
 
     def blackbody(self, joint_pars=None):
-        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__vals()
+        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__values()
 
         pars = pars if joint_pars is None else joint_pars
 
         def f_bb(_e=energy, out_ene=True):
             [amp, kt] = pars
-            out = amp * _e**2 * (np.exp(_e / kt) - 1)**-1
-            return _e * out if out_ene else out
+            res = amp * _e**2 * (np.exp(_e / kt) - 1)**-1
+            return _e * res if out_ene else res
 
         def int_bb(_range=f_ene):
             return si.quad(f_bb, _range[0], _range[1])[0] * kev_
 
         def bol_bb():
             _bol_range = [i / (1 + z) for i in f_iso]
-            return int_bb(_bol_range) * self.__time_difference()
+            return int_bb(_bol_range) * self.__get_time_difference()
 
-        return f_bb(out_ene=False) if mt == 'counts' else f_bb() if \
-            mt == 'energy' else int_bb() if mt == 'integrate' else bol_bb()
+        if mt == 'counts':
+            out = f_bb(out_ene=False)
+        elif mt == 'energy':
+            out = f_bb()
+        elif mt == 'integrate':
+            out = int_bb()
+        else:
+            out = bol_bb()
+
+        return out
 
     def cutoff_powerlaw(self, joint_pars=None):
-        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__vals()
+        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__values()
 
         pars = pars if joint_pars is None else joint_pars
 
         def f_cpl(_e=energy, out_ene=True):
             [amp, e_peak, i1, e_piv] = pars
-            out = amp * np.exp(-1 * _e * (2 + i1) * e_peak**-1) * (_e * e_piv**-1)**i1
+            res = amp * np.exp(-1 * _e * (2 + i1) * e_peak**-1) * (_e * e_piv**-1)**i1
 
-            return _e * out if out_ene else out
+            return _e * res if out_ene else res
 
         def int_cpl(_range=f_ene):
             return si.quad(f_cpl, _range[0], _range[1])[0] * kev_
 
         def bol_cpl():
             _bol_range = [i / (1 + z) for i in f_iso]
-            return int_cpl(_bol_range) * self.__time_difference()
+            return int_cpl(_bol_range) * self.__get_time_difference()
 
-        return f_cpl(out_ene=False) if mt == 'counts' else f_cpl() if \
-            mt == 'energy' else int_cpl() if mt == 'integrate' else bol_cpl()
+        func_map = {'counts': f_cpl(out_ene=False),
+                    'energy': f_cpl(),
+                    'integrate': int_cpl(),
+                    'bolometric': bol_cpl()}
+
+        return func_map[mt]
 
     def smoothly_broken_powerlaw(self, joint_pars=None):
-        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__vals()
+        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__values()
 
         pars = pars if joint_pars is None else joint_pars
 
@@ -156,22 +168,26 @@ class SpectralModels:
             beta_ = np.exp(a) + np.exp(-1 * a)
             beta = m * b_scale * np.log(0.5 * beta_)
 
-            out = amp * (_e * e_piv**-1)**b * 10**(beta - beta_piv)
+            res = amp * (_e * e_piv**-1)**b * 10**(beta - beta_piv)
 
-            return _e * out if out_ene else out
+            return _e * res if out_ene else res
 
         def int_sbpl(_range=f_ene):
             return si.quad(f_sbpl, _range[0], _range[1])[0] * kev_
 
         def bol_sbpl():
             _bol_range = [i / (1 + z) for i in f_iso]
-            return int_sbpl(_bol_range) * self.__time_difference()
+            return int_sbpl(_bol_range) * self.__get_time_difference()
 
-        return f_sbpl(out_ene=False) if mt == 'counts' else f_sbpl() if \
-            mt == 'energy' else int_sbpl() if mt == 'integrate' else bol_sbpl()
+        func_map = {'counts': f_sbpl(out_ene=False),
+                    'energy': f_sbpl(),
+                    'integrate': int_sbpl(),
+                    'bolometric': bol_sbpl()}
+
+        return func_map[mt]
 
     def band_grb_function(self, joint_pars=None):
-        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__vals()
+        energy, pars, mt, f_ene, f_iso, z, kev_ = self.__values()
 
         pars = pars if joint_pars is None else joint_pars
 
@@ -179,97 +195,103 @@ class SpectralModels:
             [amp, e_peak, i1, i2] = pars
             cond = (i1 - i2) * (2 + i1)**-1 * e_peak
 
-            out = [amp * (_en * 0.01)**i2 * np.exp(i2 - i1) * (
-                    (i1 - i2) * e_peak * 0.01 * (i1 + 2)**-1)**(i1 - i2) if
-                   _en > cond else amp * (_en * 0.01)**i1 * np.exp(
-                    -(2 + i1) * _en * e_peak**-1) for _en in _e]
+            if _e > cond:
+                res = amp * (_e * 0.01)**i2 * np.exp(i2 - i1) * ((i1 - i2) * e_peak * 0.01 * (i1 + 2)**-1)**(i1 - i2)
+            else:
+                res = amp * (_e * 0.01)**i1 * np.exp(-(2 + i1) * _e * e_peak**-1)
 
-            return _e * out if out_ene else out
+            return _e * res if out_ene else res
 
         def int_band(_range=f_ene):
             return si.quad(f_band, _range[0], _range[1])[0] * kev_
 
         def bol_band():
             _bol_range = [i / (1 + z) for i in f_iso]
-            return int_band(_bol_range) * self.__time_difference()
+            return int_band(_bol_range) * self.__get_time_difference()
 
-        return f_band(out_ene=False) if mt == 'counts' else f_band() if \
-            mt == 'energy' else int_band() if mt == 'integrate' else bol_band()
+        func_map = {'counts': f_band(out_ene=False),
+                    'energy': f_band(),
+                    'integrate': int_band(),
+                    'bolometric': bol_band()}
+
+        return func_map[mt]
 
     def pl_bb(self):
         pl_ = self.powerlaw(self.pars[:3])
         bb_ = self.blackbody(self.pars[3:])
 
-        return pl_, bb_, pl_ + bb_
+        return pl_, bb_, np.ufunc.reduce(np.add, [pl_, bb_])
 
     def cpl_pl(self):
         pl_, cpl_ = self.powerlaw(self.pars[:3]), self.cutoff_powerlaw(self.pars[3:])
 
-        return cpl_, pl_, cpl_ + pl_
+        return cpl_, pl_, np.ufunc.reduce(np.add, [cpl_, pl_])
 
     def cpl_bb(self):
         cpl_, bb_ = self.cutoff_powerlaw(self.pars[:-2]), self.blackbody(self.pars[-2:])
 
-        return cpl_, bb_, cpl_ + bb_
+        return cpl_, bb_, np.ufunc.reduce(np.add, [cpl_, bb_])
 
     def cpl_pl_bb(self):
         pl_ = self.powerlaw(self.pars[:3])
         cpl_ = self.cutoff_powerlaw(self.pars[3:-2])
         bb_ = self.blackbody(self.pars[-2:])
 
-        return cpl_, pl_, bb_, cpl_ + pl_ + bb_
+        return cpl_, pl_, bb_, np.ufunc.reduce(np.add, [cpl_, pl_, bb_])
 
     def band_pl(self):
         pl_, band_ = self.powerlaw(self.pars[:3]), self.band_grb_function(self.pars[3:])
 
-        return band_, pl_, band_ + pl_
+        return band_, pl_, np.ufunc.reduce(np.add, [band_, pl_])
 
     def band_bb(self):
         band_ = self.band_grb_function(self.pars[:-2])
         bb_ = self.blackbody(self.pars[-2:])
 
-        return band_, bb_, band_ + bb_
+        return band_, bb_, np.ufunc.reduce(np.add, [band_, bb_])
 
     def band_pl_bb(self):
         pl_ = self.powerlaw(self.pars[:3])
         band_ = self.band_grb_function(self.pars[3:-2])
         bb_ = self.blackbody(self.pars[-2:])
 
-        return band_, pl_, bb_, band_ + pl_ + bb_
+        return band_, pl_, bb_, np.ufunc.reduce(np.add, [band_, pl_, bb_])
 
     def sbpl_pl(self):
         pl_ = self.powerlaw(self.pars[:3])
         sbpl_ = self.smoothly_broken_powerlaw(self.pars[3:])
 
-        return pl_, sbpl_, sbpl_ + pl_
+        return pl_, sbpl_, np.ufunc.reduce(np.add, [sbpl_, pl_])
 
     def sbpl_bb(self):
         sbpl_ = self.smoothly_broken_powerlaw(self.pars[:-2])
         bb_ = self.blackbody(self.pars[-2:])
 
-        return sbpl_, bb_, sbpl_ + bb_
+        return sbpl_, bb_, np.ufunc.reduce(np.add, [sbpl_, bb_])
 
     def sbpl_pl_bb(self):
         pl_ = self.powerlaw(self.pars[:3])
         sbpl_ = self.smoothly_broken_powerlaw(self.pars[3:-2])
         bb_ = self.blackbody(self.pars[-2:])
 
-        return sbpl_, pl_, bb_, sbpl_ + pl_ + bb_
+        return sbpl_, pl_, bb_, np.ufunc.reduce(np.add, [sbpl_, pl_, bb_])
 
     def get_values(self):
-        model = self.model
+        model_functions = {'pl': self.powerlaw,
+                           'bb': self.blackbody,
+                           'pl_bb': self.pl_bb,
+                           'cpl': self.cutoff_powerlaw,
+                           'cpl_pl': self.cpl_pl,
+                           'cpl_bb': self.cpl_bb,
+                           'cpl_pl_bb': self.cpl_pl_bb,
+                           'band': self.band_grb_function,
+                           'band_pl': self.band_pl,
+                           'band_bb': self.band_bb,
+                           'band_pl_bb': self.band_pl_bb,
+                           'sbpl': self.smoothly_broken_powerlaw,
+                           'sbpl_pl': self.sbpl_pl,
+                           'sbpl_bb': self.sbpl_bb,
+                           'sbpl_pl_bb': self.sbpl_pl_bb}
 
-        return [self.powerlaw() if model == 'pl' else
-                self.blackbody() if model == 'bb' else
-                self.pl_bb() if model == 'pl_bb' else
-                self.cutoff_powerlaw() if model == 'cpl' else
-                self.cpl_pl() if model == 'cpl_pl' else
-                self.cpl_bb() if model == 'cpl_bb' else
-                self.cpl_pl_bb() if model == 'cpl_pl_bb' else
-                self.band_grb_function() if model == 'band' else
-                self.band_pl() if model == 'band_pl' else
-                self.band_bb() if model == 'band_bb' else
-                self.band_pl_bb() if model == 'band_pl_bb' else
-                self.smoothly_broken_powerlaw() if model == 'sbpl' else
-                self.sbpl_pl() if model == 'sbpl_pl' else
-                self.sbpl_bb() if model == 'sbpl_bb' else self.sbpl_pl_bb()][0]
+        model = self.model
+        return model_functions[model]()
